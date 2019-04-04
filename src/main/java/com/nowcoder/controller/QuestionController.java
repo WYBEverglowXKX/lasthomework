6cccee9f9786c6a1627ce560f24b236357037eff
Wang Yibin <457154735@qq.com>
2019-04-04 13:01:08 +0800

    add new function

--- src/main/java/com/nowcoder/controller/QuestionController.java
+++ src/main/java/com/nowcoder/controller/QuestionController.java
@@ -3,11 +3,9 @@ package com.nowcoder.controller;
 import com.nowcoder.async.EventModel;
 import com.nowcoder.async.EventProducer;
 import com.nowcoder.async.EventType;
-import com.nowcoder.dao.CommentDAO;
 import com.nowcoder.model.*;
 import com.nowcoder.service.*;
 import com.nowcoder.util.WendaUtil;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,10 +45,12 @@ public class QuestionController {
     @Autowired
     EventProducer eventProducer;
 
+    @Autowired
+    TopicService topicService;
+
 
     /**
-     *@Description
-     * 实现新增问题的功能，新增时要判断用户是否登录，如果没登录给他默认的ID
+     * @Description 实现新增问题的功能，新增时要判断用户是否登录，如果没登录给他默认的ID
      */
     @RequestMapping(value = "/question/add", method = {RequestMethod.POST})
     @ResponseBody
@@ -83,14 +83,14 @@ public class QuestionController {
 
 
     /**
-     *@Description
-     * 显示评论以及问题具体页面的信息
+     * @Description 显示评论以及问题具体页面的信息
      */
     @RequestMapping(value = "/question/{qid}", method = {RequestMethod.GET})
     public String questionDetail(Model model, @PathVariable("qid") int qid) {
         Question question = questionService.getById(qid);
         model.addAttribute("question", question);
-
+        Topic topic = topicService.getTopicById(question.getTopic());
+        model.addAttribute("topic", topic);
         List<Comment> commentList = commentService.getCommentsByEntity(qid, EntityType.ENTITY_QUESTION);
         List<ViewObject> comments = new ArrayList<ViewObject>();
         for (Comment comment : commentList) {
@@ -104,18 +104,18 @@ public class QuestionController {
 
             vo.set("likeCount", likeService.getLikeCount(EntityType.ENTITY_COMMENT, comment.getId()));
             vo.set("user", userService.getUser(comment.getUserId()));
-            vo.set("commentCount",commentService.getCommentCount(comment.getId(),EntityType.ENTITY_COMMENT));
+            vo.set("commentCount", commentService.getCommentCount(comment.getId(), EntityType.ENTITY_COMMENT));
 
             //  vo.set("cComments",commentService.getCommentsByEntity(qid, EntityType.ENTITY_QUESTION));
-            List<Comment>cCommentList = commentService.getCommentsByEntity(comment.getId(),EntityType.ENTITY_COMMENT);
+            List<Comment> cCommentList = commentService.getCommentsByEntity(comment.getId(), EntityType.ENTITY_COMMENT);
             List<ViewObject> cComments = new ArrayList<ViewObject>();
-            for(Comment cComment: cCommentList){
+            for (Comment cComment : cCommentList) {
                 ViewObject voc = new ViewObject();
                 voc.set("user", userService.getUser(cComment.getUserId()));
-                voc.set("cComment",cComment);
+                voc.set("cComment", cComment);
                 cComments.add(voc);
             }
-            vo.set("cComments",cComments);
+            vo.set("cComments", cComments);
             comments.add(vo);
         }
 
